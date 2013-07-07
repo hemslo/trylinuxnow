3074ddf7b66ac5aea0ae14b9695988675f1cff9e
Hemslo Wang <hemslo.wang@gmail.com>
2013-07-07 15:34:27 +0800

    Add basic user input validate.

--- terminal_judge_server/information_listener.py
+++ terminal_judge_server/information_listener.py
@@ -1,6 +1,8 @@
 import zerorpc
 import os
 import shutil
+import subprocess
+import shlex
 from config import PATH
 
 
@@ -17,18 +19,43 @@ class InformationListener(object):
         self.working_dir = os.path.join(PATH, id)
         if not os.path.exists(self.working_dir):
             os.mkdir(self.working_dir)
-        # backup
         self.__backup(id)
+
         # exec
-        # validate
-        # continue or restore
-        return True
+        self.user_input = cmd
+        self.user_result = self.__exec(cmd)
+
+        # self.__get_structure()
+
+        code = compile(expection, '<string>', 'exec')
+        exec(code, {'user_result': self.user_result,
+                    'user_input': self.user_input})
+        return {'success': True,
+                'output': self.user_result
+                }
 
     def __backup(self, id):
         self.backup_dir = os.path.join(PATH, ".%s" % id)
         if os.path.exists(self.backup_dir):
             shutil.rmtree(self.backup_dir)
         shutil.copytree(self.working_dir, self.backup_dir)
+
+    def __exec(self, command):
+        return subprocess.check_output(shlex.split(command),
+                                       cwd=self.working_dir)
+
+    def __restore(self):
+        shutil.rmtree(self.working_dir)
+        shutil.copytree(self.backup_dir, self.working_dir)
+        shutil.rmtree(self.backup_dir)
+
+    def __get_structure(self):
+        structure = {}
+        for root, dirs, files in os.walk(self.working_dir):
+            structure[root] = {}
+            structure[root]['files'] = [os.path.join(root, name) for name in files]
+            structure[root]['dirs'] = [os.path.join(root, name) for name in dirs]
+        print structure
 
 
 def main():
--- test/information_listener_test.py
+++ test/information_listener_test.py
@@ -4,7 +4,7 @@ import zerorpc
 def main():
     c = zerorpc.Client()
     c.connect("tcp://127.0.0.1:4242")
-    print c.judge('1', 2, 3)
+    print c.judge('1', 'ls', 'print user_result')
 
 
 if __name__ == '__main__':
